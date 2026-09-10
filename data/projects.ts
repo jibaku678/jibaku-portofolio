2ed84105d2d68f590982d8b280465a6ff67b7973
jibaku678 <jibaku678@gmail.com>
2026-09-10 14:30:09 +0700

    Fix syntax error on eprints link

--- data/projects.ts
+++ data/projects.ts
@@ -22,7 +22,7 @@ export const projectsData: Project[] = [
     tools: ['ArcGIS', 'Google Earth', 'SPSS', 'Epi Info'],
     role: 'Principal Researcher (Skripsi / Final Project)',
     period: '2025 – 2026',
-    link: '[http://eprints.poltekkesjogja.ac.id/id/eprint/23531](http://eprints.poltekkesjogja.ac.id/id/eprint/23531)'
+    link: 'http://eprints.poltekkesjogja.ac.id/id/eprint/23531',
     context: 'Leptospirosis remains a significant zoonotic threat in Bantul Regency. This research evaluated transmission risk by integrating epidemiological human case data with field rodent surveillance.',
     approach: [
       'Conducted rodent trapping and surveillance to evaluate Trap Success rates and species distribution.',

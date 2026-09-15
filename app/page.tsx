3a83dd09b35a4915734a350f2b4ca3ecb4c9602c
jibaku678 <jibaku678@gmail.com>
2026-09-16 00:29:44 +0700

    feat: add professional freelance data support section below projects

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import About from '@/components/About'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
 import Projects from '@/components/Projects'
+import FreelanceSection from '@/components/FreelanceSection'
 import Certifications from '@/components/Certifications'
 import Contact from '@/components/Contact'
 
@@ -16,6 +17,7 @@ export default function Home() {
       <Skills />
       <Experience />
       <Projects />
+      <FreelanceSection />
       <Certifications />
       <Contact />
     </main>
